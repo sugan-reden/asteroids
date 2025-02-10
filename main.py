3a6648bb2f69d53853a8138f893ea9984cc9d6ab
Sugan Reden <sugan-reden@pm.me>
2025-02-09 22:21:39 -0500

    Add player class and make it appear at center

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     # print("Starting asteroids!")
@@ -11,6 +12,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +20,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
