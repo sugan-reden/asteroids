c439fbe25efb8f34a395a6c6e75cd65755185be5
Sugan Reden <sugan-reden@pm.me>
2025-02-09 22:19:37 -0500

    Remove print statements at startup

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print("Starting asteroids!")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
