f7ce80db1d7fd6b4aebc3c0f6d206db1527fb013
Sugan Reden <sugan-reden@pm.me>
2025-02-23 00:41:06 -0500

    Add random spawning asteroids
    Asteroids are only of a sphere shape for now.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
 from player import Player
 
@@ -14,8 +16,12 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    asteroid_field = AsteroidField()
 
     while True:
         for event in pygame.event.get():
